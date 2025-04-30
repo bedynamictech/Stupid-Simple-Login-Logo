3d5a1dbc5d22b6b99f9a2e3602c15617b6782ebe
Kyle McClain <76190687+bedynamictech@users.noreply.github.com>
2025-04-30 11:30:36 -0700

    Updated admin menu login

--- ssll.php
+++ ssll.php
@@ -2,55 +2,49 @@
 /**
  * Plugin Name: Stupid Simple Login Logo
  * Description: Easily change the logo displayed on the Login page.
- * Version: 1.3.4
+ * Version: 1.4
  * Author: Dynamic Technologies
- * Author URI: https://bedynamic.tech
- * Plugin URI: https://github.com/bedynamictech/Stupid-Simple-Login-Logo
+ * Author URI: http://bedynamic.tech
+ * Plugin URI: https://github.com/bedynamictech/StupidSimplePlugins/tree/main/ssll
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain: ssll
- * Domain Path: /languages
  */
 
-// Prevent direct access.
-if (!defined('ABSPATH')) {
+// Prevent direct access
+if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-// Add Settings link on the Plugins page
-add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'ssll_action_links' );
-function ssll_action_links( $links ) {
-    $settings_link = '<a href="' . admin_url( 'admin.php?page=login-logo' ) . '">Settings</a>';
-    array_unshift( $links, $settings_link );
-    return $links;
-}
-
 // Define constant for option key
-define('SSLL_OPTION', 'ssll_logo_url');
+define( 'SSLL_OPTION', 'wp_logo_url' );
 
-// Load text domain for translations
-add_action('plugins_loaded', 'ssll_load_textdomain');
-function ssll_load_textdomain() {
-    load_plugin_textdomain(
-        'ssll',
-        false,
-        dirname( plugin_basename(__FILE__) ) . '/languages'
-    );
-}
-
-// Admin Menu
-add_action('admin_menu', 'ssll_add_menu');
+// Add admin menu
+add_action( 'admin_menu', 'ssll_add_menu' );
 
 function ssll_add_menu() {
-    add_menu_page(
-        'Stupid Simple',
-        'Stupid Simple',
-        'manage_options',
-        'stupidsimple',
-        'ssll_settings_page_content',
-        'dashicons-hammer',
-        99
-    );
+    global $menu;
+    $parent_exists = false;
+    foreach ( $menu as $item ) {
+        if ( ! empty( $item[2] ) && $item[2] === 'stupidsimple' ) {
+            $parent_exists = true;
+            break;
+        }
+    }
+
+    if ( ! $parent_exists ) {
+        add_menu_page(
+            'Stupid Simple',
+            'Stupid Simple',
+            'manage_options',
+            'stupidsimple',
+            function () {
+                wp_redirect( 'https://bedynamic.tech/stupid-simple/' );
+                exit;
+            },
+            'dashicons-hammer',
+            99
+        );
+    }
 
     add_submenu_page(
         'stupidsimple',
@@ -62,17 +56,52 @@ function ssll_add_menu() {
     );
 }
 
-// Settings Page Content
+// Add Settings link on Plugins page
+add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'ssll_action_links' );
+
+function ssll_action_links( $links ) {
+    $settings_link = '<a href="' . admin_url( 'admin.php?page=login-logo' ) . '">Settings</a>';
+    array_unshift( $links, $settings_link );
+    return $links;
+}
+
+// Register settings
+add_action( 'admin_init', 'ssll_register_settings' );
+
+function ssll_register_settings() {
+    register_setting( 'ssll_settings_group', SSLL_OPTION, 'esc_url_raw' );
+
+    add_settings_section(
+        'ssll_logo_section',
+        '',
+        'ssll_logo_section_callback',
+        'login-logo'
+    );
+}
+
+// Output settings section
+function ssll_logo_section_callback() {
+    $logo_url = get_option( SSLL_OPTION );
+    ?>
+    <div style="margin-top: 20px;">
+        <input type="text" id="wp_logo_url" name="<?php echo esc_attr( SSLL_OPTION ); ?>" value="<?php echo esc_attr( $logo_url ); ?>" class="regular-text" />
+        <input type="button" id="logo-btn" class="button" value="<?php echo esc_attr( $logo_url ? __( 'Remove Logo', 'ssll' ) : __( 'Upload Logo', 'ssll' ) ); ?>" />
+        <p class="description"><?php esc_html_e( 'Use the upload button, or enter the URL of an image.', 'ssll' ); ?></p>
+    </div>
+    <?php
+}
+
+// Settings page content
 function ssll_settings_page_content() {
-    wp_enqueue_script('jquery');
+    wp_enqueue_script( 'jquery' );
     wp_enqueue_media();
     ?>
     <div class="wrap">
-        <h1><?php _e('Login Logo', 'ssll'); ?></h1>
+        <h1><?php esc_html_e( 'Login Logo', 'ssll' ); ?></h1>
         <form method="post" action="options.php">
             <?php
-            settings_fields('ssll_settings_group');
-            do_settings_sections('login-logo');
+            settings_fields( 'ssll_settings_group' );
+            do_settings_sections( 'login-logo' );
             submit_button();
             ?>
         </form>
@@ -82,20 +111,21 @@ function ssll_settings_page_content() {
                 var logoBtn = $('#logo-btn');
 
                 function updateButtonText() {
-                    logoBtn.val(logoUrlInput.val() ? '<?php echo esc_js(__('Remove Logo', 'ssll')); ?>' : '<?php echo esc_js(__('Upload Logo', 'ssll')); ?>');
+                    logoBtn.val(logoUrlInput.val() ? '<?php echo esc_js( __( 'Remove Logo', 'ssll' ) ); ?>' : '<?php echo esc_js( __( 'Upload Logo', 'ssll' ) ); ?>');
                 }
 
                 updateButtonText();
 
                 logoBtn.click(function(e) {
                     e.preventDefault();
-                    if (logoBtn.val() === '<?php echo esc_js(__('Remove Logo', 'ssll')); ?>') {
+
+                    if (logoBtn.val() === '<?php echo esc_js( __( 'Remove Logo', 'ssll' ) ); ?>') {
                         $.ajax({
                             url: ajaxurl,
                             type: 'POST',
                             data: {
                                 action: 'remove_login_logo',
-                                nonce: '<?php echo wp_create_nonce('remove_login_logo_nonce'); ?>'
+                                nonce: '<?php echo wp_create_nonce( 'remove_login_logo_nonce' ); ?>'
                             },
                             success: function(response) {
                                 if (response.success) {
@@ -103,16 +133,15 @@ function ssll_settings_page_content() {
                                     updateButtonText();
                                     location.reload();
                                 } else {
-                                    alert('<?php echo esc_js(__('Error removing logo.', 'ssll')); ?>');
+                                    alert('<?php echo esc_js( __( 'Error removing logo.', 'ssll' ) ); ?>');
                                 }
                             }
                         });
                     } else {
                         var image = wp.media({
-                            title: '<?php echo esc_js(__('Select Image', 'ssll')); ?>',
+                            title: '<?php echo esc_js( __( 'Select Image', 'ssll' ) ); ?>',
                             multiple: false
-                        }).open()
-                        .on('select', function() {
+                        }).open().on('select', function() {
                             var uploaded_image = image.state().get('selection').first();
                             var image_url = uploaded_image.toJSON().url;
                             logoUrlInput.val(image_url);
@@ -127,46 +156,25 @@ function ssll_settings_page_content() {
     <?php
 }
 
-// Register Settings
-add_action('admin_init', 'ssll_register_settings');
+// AJAX handler to remove logo
+add_action( 'wp_ajax_remove_login_logo', 'remove_login_logo' );
 
-function ssll_register_settings() {
-    register_setting('ssll_settings_group', SSLL_OPTION, 'esc_url_raw');
-
-    add_settings_section(
-        'ssll_logo_section',
-        '', // Removed title to avoid extra heading
-        'ssll_logo_section_callback',
-        'login-logo'
-    );
-}
-
-// Settings Section Callback
-function ssll_logo_section_callback() {
-    $logo_url = get_option(SSLL_OPTION);
-    echo '<div style="margin-top: 20px;">';
-    echo '<input type="text" id="wp_logo_url" name="' . esc_attr(SSLL_OPTION) . '" value="' . esc_attr($logo_url) . '" class="regular-text" />';
-    echo ' <input type="button" name="logo-btn" id="logo-btn" class="button" value="' . esc_attr($logo_url ? __('Remove Logo', 'ssll') : __('Upload Logo', 'ssll')) . '" />';
-    echo '<p class="description">' . esc_html(__('Use the upload button, or enter the URL of an image.', 'ssll')) . '</p>';
-    echo '</div>';
-}
-
-// AJAX handler to remove the logo
 function remove_login_logo() {
-    check_ajax_referer('remove_login_logo_nonce', 'nonce');
-    update_option(SSLL_OPTION, '');
+    check_ajax_referer( 'remove_login_logo_nonce', 'nonce' );
+    update_option( SSLL_OPTION, '' );
     wp_send_json_success();
 }
-add_action('wp_ajax_remove_login_logo', 'remove_login_logo');
 
-// Custom WordPress admin login header logo
+// Add custom logo to login screen
+add_action( 'login_head', 'wordpress_custom_login_logo' );
+
 function wordpress_custom_login_logo() {
-    $logo_url = esc_url(get_option(SSLL_OPTION));
+    $logo_url = esc_url( get_option( SSLL_OPTION ) );
 
-    if (!empty($logo_url)) {
+    if ( ! empty( $logo_url ) ) {
         echo '<style type="text/css">
             h1 a {
-                background-image:url(' . esc_url($logo_url) . ') !important;
+                background-image: url(' . esc_url( $logo_url ) . ') !important;
                 background-size: contain !important;
                 background-repeat: no-repeat !important;
                 background-position: center center !important;
@@ -179,11 +187,10 @@ function wordpress_custom_login_logo() {
         </style>';
     }
 }
-add_action('login_head', 'wordpress_custom_login_logo');
 
-// Change login logo URL
-add_filter('login_headerurl', 'change_login_logo_url');
+// Change login logo link to site home
+add_filter( 'login_headerurl', 'change_login_logo_url' );
 
-function change_login_logo_url($url) {
-    return esc_url(home_url());
+function change_login_logo_url( $url ) {
+    return esc_url( home_url() );
 }
